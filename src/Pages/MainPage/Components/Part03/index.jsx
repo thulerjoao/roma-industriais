cda885c98ea7dae5fa9fb169cd88a5c1f0c04209
thulerjoao <thuler_lima@hotmail.com>
2023-07-12 21:38:06 -0300

    feat: updated text in part03

--- src/Pages/MainPage/Components/Part03/index.jsx
+++ src/Pages/MainPage/Components/Part03/index.jsx
@@ -8,7 +8,7 @@ const Part03 = () => {
                 <img src={ownerPic}></img>
                 <div>
                     <h2>De onde viemos</h2>
-                    <p>Rodrigo e Maria são um jovem casal empreendedor no mercado de design de interiores com móveis no estilo industrial. Rodrigo cria móveis planejados únicos, combinando aço, ferro e madeira rústica. Com o apoio de Maria no atendimento ao cliente, fundaram a Roma Industriais, oferecendo um serviço personalizado e de qualidade.  Hoje, são referência no mercado, sendo reconhecidos pela excelência e atenção aos detalhes, e mostrando que com paixão e talento é possível transformar uma ideia em realidade.</p>
+                    <p>Rodrigo e Maria são um jovem casal empreendedor no mercado de design de interiores com móveis no estilo industrial. Rodrigo cria móveis planejados únicos, combinando aço, ferro e madeira rústica. Com o apoio de Maria no atendimento ao cliente, fundaram a Roma Industriais, oferecendo um serviço personalizado e de qualidade.  Hoje, são referência no mercado, sendo reconhecidos pela excelência e atenção aos detalhes, provando que com paixão e talento é possível transformar uma ideia em realidade.</p>
                 </div>
             </section>
         </Style.Part03>
