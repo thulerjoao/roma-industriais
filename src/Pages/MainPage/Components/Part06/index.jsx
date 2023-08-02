487f60c0b1b876fd9237edf868e977748e5b780c
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-08-02 20:13:54 -0300

    feat: removed satuday in schedule

--- src/Pages/MainPage/Components/Part06/index.jsx
+++ src/Pages/MainPage/Components/Part06/index.jsx
@@ -30,7 +30,6 @@ const Part06 = () => {
                     </div>
                     <h2>Faça-nos uma visita</h2>
                     <p>Segunda a Sexta | 08:00h às 17:00h</p>
-                    <p>Sábado | 08:00h à 13:00h</p>
                     <p>---</p>
                     <p>Av. Manoel Carneiro de Menezes, 441, Ponte da Saudade, Nova Friburgo, RJ </p>
                 </html>
